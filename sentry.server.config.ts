11f212b8fbfa1889df4fa4d602a632801d1b29fb
Lakshay <tyagilakshay47@gmail.com>
2026-04-04 16:49:20 +0530

    feat(sentry): add Sentry configuration for edge and server environments
    - Created sentry.edge.config.ts for initializing Sentry in edge features.
    - Created sentry.server.config.ts for initializing Sentry on the server.
    - Configurations include DSN, tracesSampleRate, enableLogs, and sendDefaultPii.

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ecfa1d3163d971c6268a4d2ec8f40f55@o4511161399312384.ingest.de.sentry.io/4511161400950864",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
